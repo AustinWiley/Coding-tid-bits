e8ae878efc893d0cb40dce1c204dd624ec4d226d
Austin WIley <wileybeer@gmail.com>
2019-04-24 12:18:57 -0600

    adding tid bit

---
+++ tdBits.js
@@ -0,0 +1,11 @@
+const variableOne = "shoes";
+const variabletwo = "bananas";
+
+
+const myArr = ["one", "two", "three"];
+
+myArr.forEach(arr => {
+    console.log(arr + " " + variableOne);
+    console.log(arr + " " + variabletwo);
+});
+
